6f96e6bbba7710bdbc76345fbc4e6146a0e39da9
g <199296466+asdkoasak@users.noreply.github.com>
2025-03-17 00:04:09 -0300

    feat - now time shown gets properly resized every sync

--- cmd/main.go
+++ cmd/main.go
@@ -27,7 +27,7 @@ func drawText(s tcell.Screen, x1, y1, x2, y2 int, style tcell.Style, text string
 func main() {
 	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
 	boxStyle := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorReset)
-
+	
 	// Initialize screen
 	s, err := tcell.NewScreen()
 	if err != nil {
@@ -38,30 +38,38 @@ func main() {
 		log.Fatalf("%+v", err)
 		os.Exit(1)
 	}
+	
 	s.SetStyle(defStyle)
 	s.Clear()
-
-	xmax, ymax := s.Size()
-	middleX, middleY := xmax/2, ymax/2
-
-	tip := "Press CTRL+C to quit..."
-
+	
+	updateCh := make(chan struct{}, 1)
+	
+	updateCh <- struct{}{}
+	
 	go func() {
 		for {
-			time.Sleep(1 * time.Second)
-
-			currentTime := time.Now().Format("3:04:05 PM")
-
+			select {
+			case <-updateCh:
+			case <-time.After(1 * time.Second):
+			}
+			
 			s.Clear()
-			s.Sync()
+			
 			xmax, ymax := s.Size()
+			middleX, middleY := xmax/2, ymax/2
+			
+			// Draw tip
+			tip := "Press CTRL+C to quit..."
 			drawText(s, xmax-len(tip), ymax-1, xmax, ymax, tcell.StyleDefault.Foreground(tcell.ColorDarkGray).Background(tcell.ColorReset), tip)
-			drawText(s, middleX-len(currentTime)/2, middleY, xmax/2+10, ymax/2+2, boxStyle, currentTime)
-
+			
+			// Draw time
+			currentTime := time.Now().Format("3:04:05 PM")
+			drawText(s, middleX-len(currentTime)/2, middleY, xmax, ymax, boxStyle, currentTime)
+			
 			s.Show()
 		}
 	}()
-
+	
 	quit := func() {
 		maybePanic := recover()
 		s.Fini()
@@ -70,26 +78,29 @@ func main() {
 		}
 	}
 	defer quit()
-
+	
 	// Event loop
 	for {
-		// Update screen
-		s.Show()
-
 		// Poll event
 		ev := s.PollEvent()
-
+		
 		// Process event
 		switch ev := ev.(type) {
 		case *tcell.EventResize:
 			s.Sync()
+			select {
+			case updateCh <- struct{}{}:
+			default:
+			}
 		case *tcell.EventKey:
 			if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
 				return
 			} else if ev.Key() == tcell.KeyCtrlL {
 				s.Sync()
+				updateCh <- struct{}{}
 			} else if ev.Rune() == 'C' || ev.Rune() == 'c' {
 				s.Clear()
+				updateCh <- struct{}{}
 			}
 		}
 	}
